763439866c1e9c1d4c09b238072aa2664e0821ef
Gbm25 <gonzalo.busto@gmail.com>
2022-01-06 17:52:01 +0100

    Se cambia la referencia a EntityFramework por su correspondiente de "Microsoft.EntityFrameworkCore.SqlServer".

--- BankApp/LoginWindow.xaml.cs
+++ BankApp/LoginWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Configuration;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 
 namespace BankApp
 {
